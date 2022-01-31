8bba3833664fe963589c86ac4b7c99196f077959
Emmanuel208 <90685120+Emmanuel208@users.noreply.github.com>
2022-01-31 17:29:29 +0000

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Question 3
+            Console.WriteLine("Enter a total amount of time in minUTES");
+
+            var totalMinutes = Convert.ToDouble(Console.ReadLine());
+            var time = TimeSpan.FromMinutes(totalMinutes);
+            Console.WriteLine("{0:00}:{1:00}", (int)time.TotalHours, time.Minutes);
+        }
+    }
+}
