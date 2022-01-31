a18270e1ae7d78dd2a11a46d2c5086b862c699d8
Emmanuel208 <emekaobasi121@gmail.com>
2022-01-31 17:05:14 +0000

    Three commit

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -7,58 +7,17 @@ namespace ConsoleApp4
         static void Main(string[] args)
         {
 
-            //Simple Calculator
+            //question 2
+            Console.WriteLine("Enter length and width of your room");
+            Console.WriteLine("Length:"); int lenght = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Width:"); int width = Convert.ToInt32(Console.ReadLine());
+
+            //cost of carpeting per square foot
+            const int carpetingCostPerSquareFoot = 10;
+            int cost = (lenght + width) * (carpetingCostPerSquareFoot);
+
+            Console.WriteLine("Total cost : " + cost);
             
-            Console.WriteLine("What type of calculation would you like to prefrom");
-            Console.WriteLine("(1)Addition");
-            Console.WriteLine("(2)Subtraction");
-            Console.WriteLine("(3)Multiplication");
-            Console.WriteLine("(4)Division");
-
-            string option = Console.ReadLine();
-
-            if (option == "1")
-            {
-                Console.WriteLine("Enter two numbers");
-                int number1 = Convert.ToInt32(Console.ReadLine());
-                int number2 = Convert.ToInt32(Console.ReadLine());
-                int result = number1 + number2;
-
-                Console.WriteLine($"{number1} + {number2} = {result}");
-
-            }else if (option == "2")
-            {
-                Console.WriteLine("Enter two numbers");
-                int number1 = Convert.ToInt32(Console.ReadLine());
-                int number2 = Convert.ToInt32(Console.ReadLine());
-                int result = number1 - number2;
-
-                Console.WriteLine($"{number1} - {number2} = {result}");
-
-            }
-            else if (option == "3")
-            {
-                Console.WriteLine("Enter two numbers");
-                int number1 = Convert.ToInt32(Console.ReadLine());
-                int number2 = Convert.ToInt32(Console.ReadLine());
-                int result = number1 * number2;
-
-                Console.WriteLine($"{number1} * {number2} = {result}");
-
-            }
-            else if (option == "4")
-            {
-                Console.WriteLine("Enter two numbers");
-                int number1 = Convert.ToInt32(Console.ReadLine());
-                int number2 = Convert.ToInt32(Console.ReadLine());
-                double result = (number1) / (number2);
-
-                Console.WriteLine($"{number1} / {number2} = {result}");
-            }
-            else
-            {
-                Console.WriteLine("Wrong entry try again ");
-            }
 
 
 
