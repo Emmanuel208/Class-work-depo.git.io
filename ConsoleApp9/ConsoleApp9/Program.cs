c4d7c674951bcb37086bb55b65fd2d8ff72774c5
Emmanuel208 <90685120+Emmanuel208@users.noreply.github.com>
2022-02-01 14:37:11 +0000

    Add files via upload

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the random Number");
+
+            int randomNumber;
+
+            Random random = new Random();
+
+            randomNumber = random.Next(1, 11);
+
+            //*note* Print to check if random number works
+            Console.WriteLine($"Random number is {randomNumber}");
+
+            
+            bool option;
+            do {
+                Console.WriteLine("Enter number:");
+                
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+            
+                if (guess == randomNumber)
+                {
+                    option = true;
+                    Console.WriteLine("Correct");
+
+                }
+                else
+                {
+                    option = false;
+                    Console.WriteLine("worng try again");
+                }
+            }while (option == false) ;
+        }
+
+    }
+}
