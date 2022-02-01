16cb3baf88f0de365a219ded0d67619e21028fc9
Emmanuel208 <90685120+Emmanuel208@users.noreply.github.com>
2022-02-01 12:37:04 +0000

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter payment amount");
+
+            double amount = Convert.ToDouble(Console.ReadLine());
+
+            if (amount < 7.50 || amount > 49.00)
+            {
+                Console.WriteLine("Error Message");
+            }
+            else
+            {
+                Console.WriteLine("Rate ok");
+            }
+        }
+    }
+}
