b65ecd1fe8eab09a3a2d8f6ec8561fe3999e90e5
Emmanuel208 <90685120+Emmanuel208@users.noreply.github.com>
2022-02-09 09:00:47 +0000

    commit 10

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            float payRate = 17.50f;
+            Console.WriteLine("Enter pay rate:");
+
+            bool option;
+            do
+            {
+                int pay = Convert.ToInt32(Console.ReadLine());
+                if (pay < 5.65 || pay > 50)
+                {
+                    option = false;
+                    Console.WriteLine("Enter pay rate:");
+                }
+                else
+                {
+                    option = true;
+                    Console.WriteLine($"correct pay: {payRate}");
+                }
+            } while (option == false);
+        }
+    }
+}
