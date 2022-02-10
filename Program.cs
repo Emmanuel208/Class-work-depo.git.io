c171b099d9145db8177be44c241bc38fc4e83fd9
Emmanuel208 <90685120+Emmanuel208@users.noreply.github.com>
2022-02-10 13:56:15 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,21 +6,33 @@ namespace ConsoleApp12
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            int[] numbers = new int[51];
-            int sum = 0;
-            for (int i = 1; i < numbers.Length; i++)
+            Random randomGenerator = new Random();
+            int randomNumber = randomGenerator.Next(1, 11);
+            int usersGuess;
+            int amountOfGuesses = 0;
+            do
             {
-                Console.WriteLine(i);
-                
-                sum += i ;
-                Console.WriteLine("Sum is {0}", sum);
-                
-               
-            }
+                Console.WriteLine("Guess the random number");
+                usersGuess = Convert.ToInt32(Console.ReadLine());
+                if (usersGuess < randomNumber)
+                {
+                    Console.WriteLine(" Guess is on the low side");
+                }
+                else if (usersGuess > randomNumber)
+                {
+                    Console.WriteLine(" Guess is on the high side");
+                }
+                else
+                {
+                    Console.WriteLine("correct guess, sweet in the middel");
+                }
+                amountOfGuesses++;
 
-           
+            } while (usersGuess != randomNumber);
+            Console.WriteLine("You guessed {0} times :", amountOfGuesses);
+            //Console.ReadLine();
+
+
         }
     }
 }
