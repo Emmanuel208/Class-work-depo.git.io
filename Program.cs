31a3997546ff582d9530b7b0199b90c9bf61ee84
Emmanuel208 <90685120+Emmanuel208@users.noreply.github.com>
2022-02-10 13:51:15 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] numbers = new int[51];
+            int sum = 0;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                Console.WriteLine(i);
+                
+                sum += i ;
+                Console.WriteLine("Sum is {0}", sum);
+                
+               
+            }
+
+           
+        }
+    }
+}
